943db393c7a3438158477fbcccf3d2fe7d1bbfc6
akii09 <akashpise588@gmail.com>
2022-11-16 19:45:53 +0530

    APP js file missing - replaced with main.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4648,7 +4648,7 @@
     <script src="{{asset('/vendors/waypoints/jquery.waypoints.js')}}"></script>
 
     <!-- Site Javascript -->
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
     <svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1"
          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <defs>
